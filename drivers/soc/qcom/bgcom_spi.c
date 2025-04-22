aosp-mirror/kernel_msm

soc: qcom: bgcom: disable/enable interrupt on device suspend/resume
Disables/Enables the Bg interrupts on device suspend and resume.

Change-Id: Ic7131f5b6fff77fbe745fb66afabab6ca927a797
Signed-off-by: Arjun Singh <arsingh@codeaurora.org>

drivers/soc/qcom/bgcom_spi.c
@@ -929,8 +929,11 @@ static irqreturn_t bg_irq_tasklet_hndlr(int irq, void *device)
 {
 	struct bg_spi_priv *bg_spi = device;
 	/* check if call-back exists */
-	if (list_empty(&cb_head)) {
-		pr_debug("No callback registered\n");
+	if (!atomic_read(&bg_is_spi_active)) {
+		printk_ratelimited("Interrupt received in suspend state\n");
+		return IRQ_HANDLED;
+	} else if (list_empty(&cb_head)) {
+		pr_err("No callback registered\n");
 		return IRQ_HANDLED;
 	} else if (spi_state == BGCOM_SPI_BUSY) {
 		/* delay for spi to be freed */
@@ -1061,6 +1064,7 @@ static int bgcom_pm_suspend(struct device *dev)
 	if (ret == 0) {
 		bg_spi->bg_state = BGCOM_STATE_SUSPEND;
 		atomic_set(&bg_is_spi_active, 0);
+		disable_irq(bg_irq);
 	}
 	pr_info("suspended with : %d\n", ret);
 	return ret;
@@ -1076,8 +1080,11 @@ static int bgcom_pm_resume(struct device *dev)
 	clnt_handle.bg_spi = spi;
 	atomic_set(&bg_is_spi_active, 1);
 	ret = bgcom_resume(&clnt_handle);
-	if (ret == 0)
-		pr_info("Bgcom resumed\n");
+	if (ret == 0) {
+		pr_err("Bgcom resumed\n");
+		enable_irq(bg_irq);
+	}
+	pr_err("Bgcom resumed with : %d\n", ret);
 	return ret;
 }
 

inomotech-foss/linux-ag35

drivers/soc/qcom/bgcom_spi.c
@@ -922,9 +922,13 @@ EXPORT_SYMBOL(bgcom_close);
 static irqreturn_t bg_irq_tasklet_hndlr(int irq, void *device)
 {
 	struct bg_spi_priv *bg_spi = device;
+
 	/* check if call-back exists */
-	if (list_empty(&cb_head)) {
-		pr_debug("No callback registered\n");
+	if (!atomic_read(&bg_is_spi_active)) {
+		printk_ratelimited("Interrupt received in suspend state\n");
+		return IRQ_HANDLED;
+	} else if (list_empty(&cb_head)) {
+		pr_err("No callback registered\n");
 		return IRQ_HANDLED;
 	} else if (spi_state == BGCOM_SPI_BUSY) {
 		/* delay for spi to be freed */
@@ -1055,6 +1059,7 @@ static int bgcom_pm_suspend(struct device *dev)
 	if (ret == 0) {
 		bg_spi->bg_state = BGCOM_STATE_SUSPEND;
 		atomic_set(&bg_is_spi_active, 0);
+		disable_irq(bg_irq);
 	}
 	pr_info("suspended with : %d\n", ret);
 	return ret;
@@ -1071,7 +1076,8 @@ static int bgcom_pm_resume(struct device *dev)
 	atomic_set(&bg_is_spi_active, 1);
 	ret = bgcom_resume(&clnt_handle);
 	if (ret == 0)
-		pr_info("Bgcom resumed\n");
+		enable_irq(bg_irq);
+	pr_info("Bgcom resumed with : %d\n", ret);
 	return ret;
 }
 